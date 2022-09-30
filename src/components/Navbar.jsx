0945555298afc8453a6ebf907229fa5dbef2aa76
Matthew-Clay <matthewclay.dev@gmail.com>
2022-09-30 16:04:38 +0630

    finished navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+const Navbar = () => {
+  return (
+    <>
+      <div className="flex items-center justify-between p-4 z-[100] w-full absolute antialiased">
+        <h1 className="uppercase text-red-600 text-5xl font-extrabold">
+          netflix
+        </h1>
+        <div className="font-semibold text-lg cursor-pointer">
+          <button className="text-white pr-4 hover:opacity-75 hover:transition-opacity capitalize">
+            login
+          </button>
+          <button className="bg-red-600 px-6 py-2 rounded text-white hover:opacity-75 hover:transition-opacity capitalize">
+            sign up
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
